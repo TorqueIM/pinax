a676644b5fa43a30d254db38705a1ba7639ace68
Jesse <jesse.heitler@torqueim.com>
2013-03-20 11:54:13 +0000

    Fix timezone aware issue.

--- pinax/apps/account/models.py
+++ pinax/apps/account/models.py
@@ -6,6 +6,7 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils.translation import get_language_from_request, ugettext_lazy as _
+from django.utils.timezone import now
 
 from django.contrib.auth.models import User, AnonymousUser
 
@@ -48,7 +49,7 @@ class PasswordReset(models.Model):
     user = models.ForeignKey(User, verbose_name=_("user"))
     
     temp_key = models.CharField(_("temp_key"), max_length=100)
-    timestamp = models.DateTimeField(_("timestamp"), default=datetime.datetime.now)
+    timestamp = models.DateTimeField(_("timestamp"), default=now)
     reset = models.BooleanField(_("reset yet?"), default=False)
     
     def __unicode__(self):
